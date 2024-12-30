e563c8f532e0449b7d0ec36347376480f8849e68
Robert Pito Sanchez <robsanchez124@gmail.com>
2024-12-29 19:25:19 -0500

    setup tailwind and static deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+const base =
+  process.env.NODE_ENV === "productions" ? "/react-logo-typescript/" : "/";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base,
+});
