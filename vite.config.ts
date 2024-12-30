8043f4dcd75eb741c3c1a971e123aaa353917a8a
Robert Pito Sanchez <robsanchez124@gmail.com>
2024-12-29 19:33:27 -0500

    removed s from production

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 const base =
-  process.env.NODE_ENV === "productions" ? "/react-logo-typescript/" : "/";
+  process.env.NODE_ENV === "production" ? "/react-logo-typescript/" : "/";
 
 // https://vite.dev/config/
 export default defineConfig({
